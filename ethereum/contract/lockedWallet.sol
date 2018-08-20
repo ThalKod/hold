e539e469a8015dde836075ee77b9f607de64877f
ThalKod <tmarcelin@thal.tech>
2018-08-20 09:57:14 -0400

    add really  basic lockedWallet contract

---
+++ ethereum/contract/lockedWallet.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.17;
+
+contract lockedWallet{
+    
+    address public receiverAddress;
+    address public creatorAddress;
+    uint public endLockedTime;
+    uint public createdAt = now;
+    
+    constructor(address receiver, uint timestamp) public{
+        require(timestamp > now, "past timestamp");
+        receiverAddress = receiver;
+        endLockedTime = timestamp;
+        creatorAddress = msg.sender;
+    }
+    
+    function () public payable {}
+    
+    
+    function widthdraw() onlyReceiver public{
+        require(now >= endLockedTime, "time not yet elapsed");
+        msg.sender.transfer(address(this).balance);
+    }
+    
+    function getInfo()  public participant  view returns(uint, address, address, uint, uint){
+        return(address(this).balance, creatorAddress, receiverAddress, endLockedTime, createdAt);
+    }
+
+    modifier onlyReceiver{
+        require(msg.sender == receiverAddress, "must be the receiver");
+        _;
+    }
+    
+    modifier participant{
+        require(msg.sender == receiverAddress || msg.sender == creatorAddress, "not a participant");
+        _;
+    }
+    
+    
+}
