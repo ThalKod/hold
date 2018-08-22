c61286f607d4c58a0ff1eba120c7386fa54a0dee
ThalKod <tmarcelin@thal.tech>
2018-08-22 09:21:29 -0400

    add dynamic wallet creation

--- ethereum/contracts/lockedWallet.sol
+++ ethereum/contracts/lockedWallet.sol
@@ -1,34 +1,54 @@
 pragma solidity ^0.4.17;
 
-contract lockedWallet{
+
+contract FactoryWallet{
+    address[] public listOfWallets;
+    
+    function createWallet(address receiver, uint timestamp) public{
+        address newWallet = new LockedWallet(msg.sender, receiver, timestamp);
+        
+        listOfWallets.push(newWallet);
+    }
+    
+    function getWallets() public view returns( address[]){
+        return listOfWallets;
+    }
+}
+
+contract LockedWallet{
     
     address public receiverAddress;
     address public creatorAddress;
     uint public endLockedTime;
     uint public createdAt = now;
     
-    constructor(address receiver, uint timestamp) public{
+    constructor(address creator, address receiver, uint timestamp) public{
         require(timestamp > now, "past timestamp");
         receiverAddress = receiver;
         endLockedTime = timestamp;
-        creatorAddress = msg.sender;
+        creatorAddress = creator;
     }
     
     function () public payable {}
     
     
     function widthdraw() onlyReceiver public{
-        require(now >= endLockedTime, "time not yet elapsed");
+        require( now >= endLockedTime, "time not yet elapsed");
         msg.sender.transfer(address(this).balance);
     }
     
     function getInfo() public view returns(uint, address, address, uint, uint){
         return(address(this).balance, creatorAddress, receiverAddress, endLockedTime, createdAt);
     }
+    
+    function isReceiver() public view returns(bool){
+        return msg.sender == receiverAddress;
+    }
 
     modifier onlyReceiver{
         require(msg.sender == receiverAddress, "must be the receiver");
         _;
     }
-
+    
+    
 }
