d2df38aa1e6540ab786dfb03f2b438a89b5a974d
ThalKod <tmarcelin@thal.tech>
2018-08-22 09:56:31 -0400

    add nextjs

---
+++ components/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Menu extends React.Component{
+    render(){
+        return(
+            <h1>menu</h1>
+        );
+    }
+}
+
+export default Menu;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Menu from "../components/Menu";
+
+class DashboardPage extends React.Component{
+    render(){
+        return (
+            <div>
+                <Menu />
+                <h1>Dashboard Page</h1>
+            </div>  
+        );
+    }
+}
+
+export default DashboardPage;
