2934ab7baaa3e58bb5743e99d90532041a91f07d
ThalKod <tmarcelin@thal.tech>
2018-08-27 14:23:01 -0400

    add lock font style

--- components/LockFundsPage.js
+++ components/LockFundsPage.js
@@ -1,10 +1,31 @@
 import React from "react";
+import web3 from "../ethereum/web3";
+import 'react-dates/initialize';
+
+
 
 export default class LockFundsPage extends React.Component{
+
+    state = {
+        address: "",
+        date: null,
+        focused: null
+    }
+
+    async componentDidMount(){
+        const address = await web3.eth.getAccounts();
+
+        this.setState({ address: address[0]});
+    }
+
     render(){
         return(
             <div>
-                Lock Funds
+                <div className="page_header">
+                    <div className="content-container">   
+                        <h1 className="page-header__title">Lock Funds</h1>   
+                    </div>
+                </div>
             </div>
         )
     }
--- pages/lock.js
+++ pages/lock.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import LockFundsPage from "../components/LockFundsPage";
 
+
 export default ()=>(
    <Layout>
         <LockFundsPage />
